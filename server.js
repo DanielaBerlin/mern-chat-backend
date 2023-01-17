b89f5777fbbfea928e80f480346d187cad5aa190
Daniela <danielacardona2203@gmail.com>
2023-01-17 18:40:29 +0100

    chat rooms route  added to server

--- server.js
+++ server.js
@@ -1,80 +1,80 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const userRoutes = require('./routes/userRoutes')
-const mongoose = require ('mongoose');
-const dotenv = require ('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config()
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('connected to mongoDB🫱🏿‍🫲🏾')
-})
-.catch((err) => {
-    console.log(err.message)
-});
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongoDB🫱🏿‍🫲🏾');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const rooms = ['Berlin', 'Relationship-problems', 'Netflix', 'AMA'];
-const cors = require ('cors');
+const cors = require('cors');
 const Message = require('./models/Message');
 const e = require('express');
 const User = require('./models/User');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', userRoutes)
-require ('./server')
+app.use('/users', userRoutes);
+require('./server');
 
 const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
-})
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
-app.get('/rooms', (req, res)=> {
-    res.json(rooms)
-})
-
-async function getLastMessagesFromRoom(room){
-    let roomMessages = await Message.aggregate([
-        {$match:{to: room}},
-        {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
-    ])
-    return roomMessages;
+async function getLastMessagesFromRoom(room) {
+  let roomMessages = await Message.aggregate([
+    { $match: { to: room } },
+    { $group: { _id: '$date', messagesByDate: { $push: '$$ROOT' } } },
+  ]);
+  return roomMessages;
 }
 
-function sortRoomMessagesByDate(messages){
-    return messages.sort(function(a, b){
-        let date1 = a._id.split('/');
-        let date2 = b._id.split('/');
+function sortRoomMessagesByDate(messages) {
+  return messages.sort(function (a, b) {
+    let date1 = a._id.split('/');
+    let date2 = b._id.split('/');
 
-        date1 = date1[2] + date1[0] + date1[1]
-        date2 = date2[2] + date2[0] + date2[1]
+    date1 = date1[2] + date1[0] + date1[1];
+    date2 = date2[2] + date2[0] + date2[1];
 
-        return date1 < date2 ? -1 : 1
-    })
+    return date1 < date2 ? -1 : 1;
+  });
 }
 //socket connection
 
-io.on('connection', (socket)=> {
+io.on('connection', (socket) => {
+  socket.on('new-user', async () => {
+    const members = await User.find();
+    io.emit('new-user', members);
+  });
 
-    socket.on('new-user', async ()=> {
-        const members = await User.find();
-        io.emit('new-user', members)
-    })
+  socket.on('join-room', async (room) => {
+    socket.join(room);
+    let roomMessages = await getLastMessagesFromRoom(room);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages);
+  });
+});
 
-    socket.on('join-room', async(room)=>{
-        socket.join(room)
-        let roomMessages = await getLastMessagesFromRoom(room);
-        roomMessages = sortRoomMessagesByDate(roomMessages);
-        socket.emit('room-messages', roomMessages)
-    })
-})
+app.get('/rooms', (req, res) => {
+  res.json(rooms);
+});
 
 server.listen(PORT, () => {
-    console.log('💻 listening to port', PORT)
-})
-
+  console.log('💻 listening to port', PORT);
+});
